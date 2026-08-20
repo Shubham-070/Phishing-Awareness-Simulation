e17737c106db28347f5d40c3d83cfd86dd865bd1
Shubham Sharma <sharmashubham8106@gmail.com>
2026-08-21 00:43:33 +0530

    Update database.py

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Saito@129",
+        password=".....",
         database="cyberaware"
     )
 
